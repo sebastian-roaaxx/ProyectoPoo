766cba97f41f9d9a5a854349e5c26b374484380a
Sebasxroa <alvaroroa717.@gmail.com>
2026-03-09 09:26:07 -0500

    hice cambio prueba

--- src/Main/App.java
+++ src/Main/App.java
@@ -4,11 +4,14 @@ import modelo.Equipo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+     
 // ESTO ES UNA PRUEBA PARA VER SI FUNCIONA EL IMPRIMIR EN CONSOLA//
         Equipo equipo1 = new Equipo("HP", "Laptop", "Activo", "4500");
 
         equipo1.registrar();
+
+
+        System.out.println("HOLA CAMBIASTE EL CODIGO");
     }
     
 
